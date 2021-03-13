27aae061ac5c9206a529db3150a1fa825ae09fbd
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-13 10:55:23 +0000

    cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { Grid } from '@material-ui/core';
 import React from 'react';
+import Header from './Header';
+import Content from './Content';
 
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <Grid container direction='column'>
+      <Grid item>
+        <Header />
+      </Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={12} sm={8}>
+          <Content />
+        </Grid>
+        <Grid item xs={0} sm={2} />
+      </Grid>
+    </Grid>
+  );
 };
 
 export default App;
