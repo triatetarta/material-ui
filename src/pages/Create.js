1666bc7efceca52ff1a21188ba6b875e4f70f33e
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-15 09:01:54 +0000

    text color

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -4,7 +4,12 @@ import { Typography } from '@material-ui/core';
 const Create = () => {
   return (
     <>
-      <Typography variant='h6' component='h2' gutterBottom>
+      <Typography
+        color='textSecondary'
+        variant='h6'
+        component='h2'
+        gutterBottom
+      >
         Create a New Note
       </Typography>
     </>
